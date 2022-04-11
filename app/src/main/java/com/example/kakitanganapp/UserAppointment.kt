c7296113dab1a5086e45b9bf9ae052cd378d70d3
xyyy213 <cheahx-pm19@student.tarc.edu.my>
2022-04-12 02:09:22 +0800

    xy 1.0

---
+++ app/src/main/java/com/example/kakitanganapp/UserAppointment.kt
@@ -0,0 +1,61 @@
+package com.example.kakitanganapp
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.format.DateFormat
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import android.widget.TimePicker
+
+class UserAppointment : AppCompatActivity() , DatePickerDialog.OnDateSetListener,
+    TimePickerDialog.OnTimeSetListener {
+    lateinit var textView: TextView
+    lateinit var button: Button
+    var day = 0
+    var month: Int = 0
+    var year: Int = 0
+    var hour: Int = 0
+    var minute: Int = 0
+    var myDay = 0
+    var myMonth: Int = 0
+    var myYear: Int = 0
+    var myHour: Int = 0
+    var myMinute: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_appointment)
+        textView = findViewById(R.id.textView_datetime)
+        button = findViewById(R.id.btnPick)
+        button.setOnClickListener {
+            val calendar: Calendar = Calendar.getInstance()
+            day = calendar.get(Calendar.DAY_OF_MONTH)
+            month = calendar.get(Calendar.MONTH)
+            year = calendar.get(Calendar.YEAR)
+            val datePickerDialog =
+                DatePickerDialog(this@UserAppointment, this@UserAppointment, year, month, day)
+            datePickerDialog.show()
+        }
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        myDay = day
+        myYear = year
+        myMonth = month
+        val calendar: Calendar = Calendar.getInstance()
+        hour = calendar.get(Calendar.HOUR)
+        minute = calendar.get(Calendar.MINUTE)
+        val timePickerDialog = TimePickerDialog(this@UserAppointment, this@UserAppointment, hour, minute,
+            DateFormat.is24HourFormat(this))
+        timePickerDialog.show()
+    }
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        myHour = hourOfDay
+        myMinute = minute
+        textView.text = "Year: " + myYear + "\n" + "Month: " + myMonth + "\n" + "Day: " + myDay + "\n" + "Hour: " + myHour + "\n" + "Minute: " + myMinute
+    }
+}
