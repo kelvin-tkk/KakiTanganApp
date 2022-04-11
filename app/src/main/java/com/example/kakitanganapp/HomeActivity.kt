4b6226e4e5bc6a8d8be1dab2c013478ecb244ae4
Kelvin TKK <kelvintkk-pm19@student.tarc.edu.my>
2022-04-12 06:57:01 +0800

    kk 1.5

--- app/src/main/java/com/example/kakitanganapp/HomeActivity.kt
+++ app/src/main/java/com/example/kakitanganapp/HomeActivity.kt
@@ -1,11 +1,74 @@
 package com.example.kakitanganapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
+import androidx.cardview.widget.CardView
+import androidx.core.view.GravityCompat
 
-class HomeActivity : AppCompatActivity() {
+
+class HomeActivity : AppCompatActivity(), View.OnClickListener , NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawer : DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        toolbar = findViewById(R.id.tlbHome)
+        drawer = findViewById(R.id.drawer_layout)
+        val navigationview : NavigationView = findViewById(R.id.nav_view)
+        navigationview.setNavigationItemSelectedListener(this)
+        val toggle : ActionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_profile -> {
+                startActivity(
+                    Intent(
+                        this,
+                        ContactsContract.Profile::class.java
+                    )
+                )
+            }
+        }
+        return true
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.cvLogout -> {
+                finish()
+            }
+        }
     }
 }
