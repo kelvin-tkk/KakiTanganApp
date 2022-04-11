be6639f8cc26930b582902457345dc3e04ea451c
Kelvin TKK <kelvintkk-pm19@student.tarc.edu.my>
2022-04-11 23:06:18 +0800

    kk 1.3

--- app/src/main/java/com/example/kakitanganapp/UserProfile.kt
+++ app/src/main/java/com/example/kakitanganapp/UserProfile.kt
@@ -1,11 +1,34 @@
 package com.example.kakitanganapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class UserProfile : AppCompatActivity() {
+class UserProfile : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var btnEditProf : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+        btnEditProf = findViewById(R.id.btnEditProf)
+        btnEditProf.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.btnEditProf->{
+                startActivity(
+                    Intent(
+                        this,
+                        ProfileEdit::class.java
+                    ).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                )
+            }
+        }
     }
 }
