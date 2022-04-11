b0bb56426640617434344f64d516daa2bb6ca4ef
Kelvin TKK <kelvintkk-pm19@student.tarc.edu.my>
2022-04-12 05:21:40 +0800

    kk 1.4

--- app/src/main/java/com/example/kakitanganapp/MainActivity.kt
+++ app/src/main/java/com/example/kakitanganapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.kakitanganapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) //set theme to light
+        Handler().postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK))
+            finish()
+        },2000)
     }
 }
