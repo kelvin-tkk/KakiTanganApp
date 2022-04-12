d9a7a4ba001eee3f16850e34818f50633f86c876
xyyy213 <cheahx-pm19@student.tarc.edu.my>
2022-04-12 22:33:25 +0800

    xy 3.0

---
+++ app/src/main/java/com/example/kakitanganapp/Firestore.kt
@@ -0,0 +1,11 @@
+package com.example.kakitanganapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Firestore : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firestore)
+    }
+}
---
+++ app/src/main/java/com/example/kakitanganapp/User.kt
@@ -0,0 +1,9 @@
+package com.example.kakitanganapp
+
+data class User(
+    val phoneNum: String,
+    val name: String,
+    val gender: Char,
+    val address: String,
+    val password: String
+)
