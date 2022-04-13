8a13e93ee96d223402700bc737408cc8c9577ac6
xyyy213 <cheahx-pm19@student.tarc.edu.my>
2022-04-13 10:02:38 +0800

    xy 4.0

--- app/src/main/java/com/example/kakitanganapp/Payment.kt
+++ app/src/main/java/com/example/kakitanganapp/Payment.kt
@@ -3,16 +3,19 @@ package com.example.kakitanganapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Payment : AppCompatActivity() {
+    lateinit var payCard: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
-        startActivity(
-            Intent(
-                this,
-                CardDetail::class.java
-            )
-        )
+
+        payCard = findViewById(R.id.btnPayCard)
+        payCard.setOnClickListener {
+            val intent = Intent(this,CardDetail::class.java)
+            startActivity(intent)
+        }
     }
 }
