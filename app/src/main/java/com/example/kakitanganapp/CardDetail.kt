970bb72956de56791d00293e185645f8bfdc32ab
xyyy213 <cheahx-pm19@student.tarc.edu.my>
2022-04-13 10:09:08 +0800

    xy 4.0

--- app/src/main/java/com/example/kakitanganapp/CardDetail.kt
+++ app/src/main/java/com/example/kakitanganapp/CardDetail.kt
@@ -3,16 +3,19 @@ package com.example.kakitanganapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CardDetail : AppCompatActivity() {
+    lateinit var makeDonate: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_detail)
-        startActivity(
-            Intent(
-                this,
-                ConfirmPayment::class.java
-            )
-        )
+
+        makeDonate = findViewById(R.id.btnMakeDonate)
+        makeDonate.setOnClickListener {
+            val intent = Intent(this,ConfirmPayment::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/kakitanganapp/PaymentSuccessful.kt
+++ app/src/main/java/com/example/kakitanganapp/PaymentSuccessful.kt
@@ -3,16 +3,19 @@ package com.example.kakitanganapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PaymentSuccessful : AppCompatActivity() {
+    lateinit var successNxt: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_successful)
-        startActivity(
-            Intent(
-                this,
-                PaymentDetail1::class.java
-            )
-        )
+
+        successNxt = findViewById(R.id.btnSuccessNxt)
+        successNxt.setOnClickListener {
+            val intent = Intent(this,PaymentDetail1::class.java)
+            startActivity(intent)
+        }
     }
 }
